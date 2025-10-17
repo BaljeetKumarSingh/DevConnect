db4f6091c9daff9a81d376c0c74368aefbc60be8
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-10-17 15:53:47 +0530

    HTTP Methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 
 const app = express();
 
-// handle request
-// app.use((req, res) => {
-//   res.send("Welcome from the server!");
-// });
+// handling different http request
 
-// handle routes
-app.use("/test", (req, res) => {
-  res.send("Welcome to the test!");
+app.use("/user", (req, res) => {
+  res.send("Hello user!");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Baljeet", lastName: "Singh" });
+});
+
+app.post("/user", (req, res) => {
+  // saving data to db
+  res.send("Data successfully saved to Database!");
+});
+
+app.delete("/user", (req, res) => {
+  // delete user from the database
+  res.send("User is deleted from the database");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Database updated successfully");
 });
 
 app.listen(3000, () => {
