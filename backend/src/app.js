b16864bd14dd58ed3f2c298c953afc1b0f0dfc95
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-10-18 01:10:30 +0530

    Playing with route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,29 +2,39 @@ const express = require("express");
 
 const app = express();
 
-// handling different http request
+// playing out with multiple route handler
 
-app.use("/user", (req, res) => {
-  res.send("Hello user!");
-});
+// route handle signature
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Baljeet", lastName: "Singh" });
-});
+// app.use("/path", rH, rH1, rH2, rH3, rH4, rH5);
+// app.use("/path", [rH, rH1, rH2], rH3, rH4, rH5);
+// app.use("/path", rH, [rH1, rH2], rH3, rH4, rH5); // mix-match array
 
-app.post("/user", (req, res) => {
-  // saving data to db
-  res.send("Data successfully saved to Database!");
-});
-
-app.delete("/user", (req, res) => {
-  // delete user from the database
-  res.send("User is deleted from the database");
-});
-
-app.put("/user", (req, res) => {
-  res.send("Database updated successfully");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5");
+    res.send("5th response");
+    // next(); // if we call this without sending response, cannot GET /user
+    // b/c it is waiting for the next route handler which is not preset.
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running successfully on port 3000...");
