cffca17ae61abb8b30bf646dfe1b4de086d37e93
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-10-22 18:03:40 +0530

    Exploring validator library

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,8 +12,21 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       unique: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email: " + value);
+        }
+      },
     },
-    password: { type: String, required: true },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password");
+        }
+      },
+    },
     age: { type: Number, min: 18 },
     gender: {
       type: String,
