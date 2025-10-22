9dcda6d96d20838785388f52d29664661a441fbb
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-10-22 21:06:15 +0530

    Created Login API

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const { loadEnvFile } = require("node:process");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 loadEnvFile();
 const app = express();
 
@@ -15,12 +18,59 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   try {
+    // validate the data
+
+    validateSignUpData(req);
+
+    // Encrypt the password
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
     // create an instance of user model
-    const user = new User(req.body);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("A user Added Successfully!");
   } catch (err) {
-    res.status(400).send(`User is not added!: ${err}`);
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    // get data from req.body
+    const { emailId, password } = req.body;
+
+    // validate email
+    if (!validator.isEmail(emailId)) {
+      throw new Error("Incorrect Email");
+    }
+
+    // authenticate email and password
+    // authenticate email
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credential");
+    }
+
+    // authenticate password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid Credential");
+    } else {
+      res.send("LoggedIn Successfully!");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
   }
 });
 
