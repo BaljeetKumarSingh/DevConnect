ae289400136877c5ca7630e02642033dd8cfba70
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-11-07 17:56:43 +0530

    Created user connections API

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequests");
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName age gender about skills photoUrl";
 // get all the pending connection request for logged in user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -10,13 +11,38 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", "firstName lastName about skills photoUrl");
-
-    // }).populate("fromUserId", ["firstName", "lastName", "about", "skills", "photoUrl"]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.json({
       message: "Requests fetched successfully!",
       data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+// get all my connections, wheather that are accepted by other or accepted by me
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const acceptedRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      status: "accepted",
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA); // here we are populating with both the data
+
+    // BugFix: filter out the required connections i.e. other than loggedInUser
+    const data = acceptedRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.status(200).json({
+      message: "Accepted request fetched sucessfully!",
+      connections: data,
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
