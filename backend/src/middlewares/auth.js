2a801e83e75bc015c64308aa6214992c86166aee
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-11-12 14:07:17 +0530

    Added feature to redirect to login page if not authenticated

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -9,7 +9,7 @@ const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token) {
-      throw new Error("Token is not valid!");
+      return res.status(401).send("Please Login!");
     }
 
     // verify the token contain in the cookie
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
   return (
     <div className="navbar bg-base-300 shadow-blue-300 shadow-md">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">DevConnect</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          DevConnect
+        </Link>
       </div>
       <div className="flex gap-2">
         {user && (
@@ -30,10 +32,10 @@ const NavBar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
